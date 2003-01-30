f8f29757f97ae57c8510dd939c5dc2d624ff6a87
joewhaley <joewhaley@users.noreply.github.com>
2003-01-30 06:22:19 +0000

    Fixed some javadoc, other small changes.

--- JavaBDD/org/sf/javabdd/BDDException.java
+++ JavaBDD/org/sf/javabdd/BDDException.java
@@ -1,7 +1,10 @@
 package org.sf.javabdd;
 
 /**
+ * An exception caused by an invalid BDD operation.
+ * 
  * @author John Whaley
+ * @version $Id$
  */
 public class BDDException extends Exception {
     public BDDException() {
