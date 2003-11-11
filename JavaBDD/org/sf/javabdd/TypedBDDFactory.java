3af5ced38a58579ab715bb766c4dc11988380d68
joewhaley <joewhaley@users.noreply.github.com>
2003-11-11 19:50:04 +0000

    Small changes: use bdd environment variable to specify factory, etc.

--- JavaBDD/org/sf/javabdd/TypedBDDFactory.java
+++ JavaBDD/org/sf/javabdd/TypedBDDFactory.java
@@ -36,7 +36,12 @@ public class TypedBDDFactory extends BDDFactory {
     }
     
     public static BDDFactory init(int nodenum, int cachesize) {
-        BDDFactory a = BuDDyFactory.init(nodenum, cachesize);
+        BDDFactory a;
+        String factoryName = System.getProperty("bdd");
+        if (factoryName != null && factoryName.equals("typed"))
+            a = BuDDyFactory.init(nodenum, cachesize);
+        else
+            a = BDDFactory.init(nodenum, cachesize);
         return new TypedBDDFactory(a);
     }
     
@@ -1075,7 +1080,8 @@ public class TypedBDDFactory extends BDDFactory {
          * @see org.sf.javabdd.BDDPairing#set(int, int)
          */
         public void set(int oldvar, int newvar) {
-            throw new BDDException();
+            pairing.set(oldvar, newvar);
+            //throw new BDDException();
         }
 
         /* (non-Javadoc)
