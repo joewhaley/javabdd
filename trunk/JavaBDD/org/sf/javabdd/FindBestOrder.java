45b306de6038e0d3437e49d5178243b0a05cdec3
joewhaley <joewhaley@6f871624-6415-0410-bc4f-e03147e02fdd>
2004-08-27 01:02:35 +0000

    *** empty log message ***

--- trunk/JavaBDD/org/sf/javabdd/FindBestOrder.java
+++ trunk/JavaBDD/org/sf/javabdd/FindBestOrder.java
@@ -114,13 +114,17 @@ public class FindBestOrder {
         } catch (InterruptedException x) {
         }
         t.stop();
+        Thread.yield(); // Help ThreadDeath exception to propagate.
         if (t.totalTime == Long.MAX_VALUE) {
+            System.out.println("Thread taking too long, aborted.");
+            System.out.print("Free memory: "+Runtime.getRuntime().freeMemory());
             b1 = null;
             b2 = null;
             b3 = null;
             bdd = null;
             newbdd = true;
             System.gc();
+            System.out.println(" bytes -> "+Runtime.getRuntime().freeMemory()+" bytes");
         }
         if (t.time < bestCalcTime) {
             bestOrder = varOrder;
