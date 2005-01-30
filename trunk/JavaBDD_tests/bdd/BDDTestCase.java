67a0d37425a24e54b3403b9fe7b1cb824b94d9ec
joewhaley <joewhaley@6f871624-6415-0410-bc4f-e03147e02fdd>
2005-01-30 14:43:40 +0000

    Added new microBDD factory: each BDD node is only 4 words.

--- trunk/JavaBDD_tests/bdd/BDDTestCase.java
+++ trunk/JavaBDD_tests/bdd/BDDTestCase.java
@@ -21,6 +21,7 @@ public abstract class BDDTestCase extends TestCase implements Iterator {
     public static final String[] factoryNames = {
         "net.sf.javabdd.BuDDyFactory",
         "net.sf.javabdd.JFactory",
+        "net.sf.javabdd.MicroFactory",
         //"net.sf.javabdd.CUDDFactory",
         //"net.sf.javabdd.CALFactory",
         //"net.sf.javabdd.JDDFactory",
