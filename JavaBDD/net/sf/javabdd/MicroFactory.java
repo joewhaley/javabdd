6aad9856337a178ab252e6a4801cb7a5a07f9755
joewhaley <joewhaley@users.noreply.github.com>
2005-06-29 07:54:32 +0000

    Removed stale import.

--- JavaBDD/net/sf/javabdd/MicroFactory.java
+++ JavaBDD/net/sf/javabdd/MicroFactory.java
@@ -8,7 +8,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.io.BufferedReader;
